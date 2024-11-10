066713ab597a5e8b344d07573f5e6e5524d2c0cb
svenvd <svenvd.github@gmail.com>
2024-11-10 01:11:10 +0100

    refer to opensurv

--- surveillance/core/util/config.py
+++
@@ -1,4 +0,0 @@
-import yaml
-#Separate config.py to we can import it to be used as global config, without having it to be passed to every function/class
-with open("../etc/general.yml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile, Loader=yaml.Loader)
